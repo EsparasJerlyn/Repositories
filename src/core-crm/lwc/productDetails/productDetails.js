18278d1e4fff944de9b13e7b7425bbb244b796a5
Roy Regala <roy.regala@qut.edu.au>
2021-12-15 05:31:54 +0000

    Merged in feature/IT1Feb/JIRA-DEPP-618 (pull request #621)
    Feature/IT1Feb/JIRA DEPP-618
    
    * updated productdetails
    
    * added comments
    
    * merged conflicts
    
    * removed commonutil
    
    * updated comments and added data checking on banner
    
    * updated documentation of testclass
    
    * updated placeholder for options
    
    * updated testclass
    
    * added documentation
    
    * Merge branch 'release/IT1Feb' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-618
    
    
    Approved-by: Rhea Torres

---
+++ src/core-crm/lwc/productDetails/productDetails.js
@@ -0,0 +1,332 @@
+/**
+ * @description A LWC component to display product details
+ *
+ * @see ../classes/ProductDetailsCtrl.cls
+ * @see productDetailsDisplay
+ * @author Accenture
+ * 
+ * @history
+ *    | Developer                 | Date                  | JIRA                 | Change Summary                               |
+      |---------------------------|-----------------------|----------------------|----------------------------------------------|
+      | xenia.gaerlan             | November 2, 2021      | DEPP-618             | ProductController.cls, Study OPE             |
+      |                           |                       |                      | Program UI Layout                            |
+      | xenia.gaerlan             | Novemver 11, 2021     | DEPP-618             | Prescribed Program, Flexible Program         | 
+      |                           |                       |                      | Course Unit Program UI Layouts               |
+      | xenia.gaerlan             | Novemver 18, 2021     | DEPP-618             | GetProgramTypeCtrl                           | 
+      | roy.nino.s.regala         | December 6, 2021      | DEPP-116             | Removed unsused code and added field mapping |
+ */
+import { LightningElement, wire, api } from 'lwc';
+
+import communityId from '@salesforce/community/Id';
+import getProductDetails from '@salesforce/apex/ProductDetailsCtrl.getProductRelatedRecords'
+import getProduct from '@salesforce/apex/B2BGetInfo.getProduct';
+import getCartSummary from '@salesforce/apex/B2BGetInfo.getCartSummary';
+import addToCart from '@salesforce/apex/B2BGetInfo.addToCart';
+import createAndAddToList from '@salesforce/apex/B2BGetInfo.createAndAddToList';
+import getProductPrice from '@salesforce/apex/B2BGetInfo.getProductPrice';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { resolve } from 'c/cmsResourceResolver';
+
+/**
+ * A detailed display of a product.
+ * This outer component layer handles data retrieval and management, as well as projection for internal display components.
+ */
+export default class ProductDetails extends LightningElement {
+    /**
+     * Gets the effective account - if any - of the user viewing the product.
+     *
+     * @type {string}
+     */
+    @api
+    get effectiveAccountId() {
+        return this._effectiveAccountId;
+    }
+
+    /**
+     * Sets the effective account - if any - of the user viewing the product
+     * and fetches updated cart information
+     */
+    set effectiveAccountId(newId) {
+        this._effectiveAccountId = newId;
+        this.updateCartInformation();
+    }
+
+    /**
+     * Gets or sets the unique identifier of a product.
+     *
+     * @type {string}
+     */
+    @api
+    recordId;
+
+    /**
+     * Gets or sets the custom fields to display on the product
+     * in a comma-separated list of field names
+     *
+     * @type {string}
+     */
+    @api
+    customDisplayFields;
+
+    /**
+     * The cart summary information
+     *
+     * @type {ConnectApi.CartSummary}
+     * @private
+     */
+    cartSummary;
+
+    /**
+     * The full product information retrieved.
+     *
+     * @type {ConnectApi.ProductDetail}
+     * @private
+     */
+    @wire(getProduct, {
+        communityId: communityId,
+        productId: '$recordId',
+        effectiveAccountId: '$resolvedEffectiveAccountId'
+    })
+    product;
+
+    /**
+     * The price of the product for the user, if any.
+     *
+     * @type {ConnectApi.ProductPrice}
+     * @private
+     */
+    @wire(getProductPrice, {
+        communityId: communityId,
+        productId: '$recordId',
+        effectiveAccountId: '$resolvedEffectiveAccountId'
+    })
+    productPrice;
+
+    /**
+     * The price of the product for the user, if any.
+     *
+     * @type {wrapperClass}
+     * @private
+     */
+    @wire(getProductDetails,{
+        productId: '$recordId'
+    })
+    productDetails;
+
+
+    /**
+     * The connectedCallback() lifecycle hook fires when a component is inserted into the DOM.
+     */
+    connectedCallback() {
+        this.updateCartInformation();
+    }
+
+    /**
+     * Gets the normalized effective account of the user.
+     *
+     * @type {string}
+     * @readonly
+     * @private
+     */
+    get resolvedEffectiveAccountId() {
+        const effectiveAccountId = this.effectiveAccountId || '';
+        let resolved = null;
+
+        if (
+            effectiveAccountId.length > 0 &&
+            effectiveAccountId !== '000000000000000'
+        ) {
+            resolved = effectiveAccountId;
+        }
+        return resolved;
+    }
+
+    /**
+     * Gets whether product information has been retrieved for display.
+     *
+     * @type {Boolean}
+     * @readonly
+     * @private
+     */
+    get hasProduct() {
+        return this.product.data !== undefined;
+    }
+
+    /**
+     * Gets the normalized, displayable product information for use by the display components.
+     *
+     * @readonly
+     */
+    get displayableProduct() {
+        return {
+            categoryPath: this.product.data.primaryProductCategoryPath.path.map(
+                (category) => ({
+                    id: category.id,
+                    name: category.name
+                })
+            ),
+            overview: this.product.data.fields.Overview__c?this.htmlDecode(this.product.data.fields.Overview__c):'',
+            evolveWithQutex: this.product.data.fields.Evolve_with_QUTeX__c?this.htmlDecode(this.product.data.fields.Evolve_with_QUTeX__c):'',
+            whoShouldParticipate:  this.product.data.fields.Who_Should_Participate__c?this.htmlDecode(this.product.data.fields.Who_Should_Participate__c):'',
+            coreConcepts: this.product.data.fields.Core_Concepts__c?this.htmlDecode(this.product.data.fields.Core_Concepts__c):'',
+            moreDetails: this.product.data.fields.More_Details__c?this.htmlDecode(this.product.data.fields.More_Details__c):'',
+            image: {
+                alternativeText: this.product.data.defaultImage.alternativeText,
+                url: resolve(this.product.data.defaultImage.url)
+            },
+            name: this.product.data.fields.Name,
+            price: {
+                currency: ((this.productPrice || {}).data || {})
+                    .currencyIsoCode,
+                negotiated: ((this.productPrice || {}).data || {}).unitPrice
+            }
+        };
+    }
+
+    /**
+     * Gets courseOffering related to the product
+     *
+     * @readonly
+     */
+    get courseOfferingList(){
+        return this.productDetails.data?this.productDetails.data.courseOfferingList:[];
+    }
+
+    /**
+     * Gets priceBookEntries related to the product
+     *
+     * @readonly
+     */
+    get priceBookEntryList(){
+        return this.productDetails.data?this.productDetails.data.priceBookEntryList:[];
+    }
+
+    /**
+     * Gets whether the cart is currently locked
+     *
+     * Returns true if the cart status is set to either processing or checkout (the two locked states)
+     *
+     * @readonly
+     */
+    get _isCartLocked() {
+        const cartStatus = (this.cartSummary || {}).status;
+        return cartStatus === 'Processing' || cartStatus === 'Checkout';
+    }
+
+    /**
+     * Returns the html format of text
+     */
+    htmlDecode(input){
+        var e = document.createElement('div');
+        e.innerHTML = input;
+        return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+    }
+
+    /**
+     * Handles a user request to add the product to their active cart.
+     * On success, a success toast is shown to let the user know the product was added to their cart
+     * If there is an error, an error toast is shown with a message explaining that the product could not be added to the cart
+     *
+     * Toast documentation: https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.use_toast
+     *
+     * @private
+     */
+    addToCart(event) {
+        addToCart({
+            communityId: communityId,
+            productId: this.recordId,
+            quantity: event.detail.quantity,
+            effectiveAccountId: this.resolvedEffectiveAccountId
+        })
+            .then(() => {
+                this.dispatchEvent(
+                    new CustomEvent('cartchanged', {
+                        bubbles: true,
+                        composed: true
+                    })
+                );
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Your cart has been updated.',
+                        variant: 'success',
+                        mode: 'dismissable'
+                    })
+                );
+            })
+            .catch(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message:
+                            '{0} could not be added to your cart at this time. Please try again later.',
+                        messageData: [this.displayableProduct.name],
+                        variant: 'error',
+                        mode: 'dismissable'
+                    })
+                );
+            });
+    }
+
+    /**
+     * Handles a user request to add the product to a newly created wishlist.
+     * On success, a success toast is shown to let the user know the product was added to a new list
+     * If there is an error, an error toast is shown with a message explaining that the product could not be added to a new list
+     *
+     * Toast documentation: https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.use_toast
+     *
+     * @private
+     */
+    createAndAddToList() {
+        let listname = this.product.data.primaryProductCategoryPath.path[0]
+            .name;
+        createAndAddToList({
+            communityId: communityId,
+            productId: this.recordId,
+            wishlistName: listname,
+            effectiveAccountId: this.resolvedEffectiveAccountId
+        })
+            .then(() => {
+                this.dispatchEvent(new CustomEvent('createandaddtolist'));
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: '{0} was added to a new list called "{1}"',
+                        messageData: [this.displayableProduct.name, listname],
+                        variant: 'success',
+                        mode: 'dismissable'
+                    })
+                );
+            })
+            .catch(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message:
+                            '{0} could not be added to a new list. Please make sure you have fewer than 10 lists or try again later',
+                        messageData: [this.displayableProduct.name],
+                        variant: 'error',
+                        mode: 'dismissable'
+                    })
+                );
+            });
+    }
+
+    /**
+     * Ensures cart information is up to date
+     */
+    updateCartInformation() {
+        getCartSummary({
+            communityId: communityId,
+            effectiveAccountId: this.resolvedEffectiveAccountId
+        })
+            .then((result) => {
+                this.cartSummary = result;
+            })
+            .catch((e) => {
+                // Handle cart summary error properly
+                // For this sample, we can just log the error
+                console.log(e);
+            });
+    }
+}
---
+++ src/core-crm/lwc/productDetailsDisplay/productDetailsDisplay.js
@@ -0,0 +1,363 @@
+/**
+ * @description A LWC component to display product details
+ *
+ * @see ../classes/ProductController.cls
+ * @see productDetailsDisplay
+ * @author Accenture
+ * 
+ * @history
+ *    | Developer                 | Date                  | JIRA                 | Change Summary                               |
+      |---------------------------|-----------------------|----------------------|----------------------------------------------|
+      | xenia.gaerlan             | November 2, 2021      | DEPP-618             | ProductController.cls, Study OPE             |
+      |                           |                       |                      | Program UI Layout                            |
+      | xenia.gaerlan             | Novemver 11, 2021     | DEPP-618             | Prescribed Program, Flexible Program         | 
+      |                           |                       |                      | Course Unit Program UI Layouts               |
+      | xenia.gaerlan             | Novemver 18, 2021     | DEPP-618             | GetProgramTypeCtrl                           | 
+      | roy.nino.s.regala         | December 6, 2021      | DEPP-116             | Removed unsused code and added field mapping |                                     |
+ */
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+/* Images */
+import Facilitator from '@salesforce/resourceUrl/Facilitator';
+import Recently1 from '@salesforce/resourceUrl/Recently1';
+import Recently2 from '@salesforce/resourceUrl/Recently2';
+import Recently3 from '@salesforce/resourceUrl/Recently3';
+
+// A fixed entry for the home page.
+const homePage = {
+    name: 'Home',
+    type: 'standard__namedPage',
+    attributes: {
+        pageName: 'home'
+    }
+};
+
+/**
+ * An organized display of product information.
+ *
+ * @fires ProductDetailsDisplay#addtocart
+ * @fires ProductDetailsDisplay#createandaddtolist
+ */
+export default class ProductDetailsDisplay extends NavigationMixin(
+    LightningElement
+) {
+    facilitator = Facilitator;
+    image1 = Recently1;
+    image2 = Recently2;
+    image3 = Recently3;
+    selectedOfferingId = '';
+    selectedDate;
+    //@wire(getWrapperProduct, { prodName: '$prodName'})
+    //productDetails;
+
+    //@wire(getWrapperContact, { strName: '$strName'})
+    //wrapperContacts
+
+    @api recordId;
+    @api objectApiName;
+
+    //product fields to display
+    @api overview;
+    @api whoShouldParticipate;
+    @api coreConcepts;
+    @api moreDetails;
+    @api evolveWithQutex;
+
+    @api courseOfferings;
+    @api priceBookEntries;
+
+    /**
+     * A product image.
+     * @typedef {object} Image
+     *
+     * @property {string} url
+     *  The URL of an image.
+     *
+     * @property {string} alternativeText
+     *  The alternative display text of the image.
+     */
+
+    /**
+     * A product category.
+     * @typedef {object} Category
+     *
+     * @property {string} id
+     *  The unique identifier of a category.
+     *
+     * @property {string} name
+     *  The localized display name of a category.
+     */
+
+    /**
+     * A product price.
+     * @typedef {object} Price
+     *
+     * @property {string} negotiated
+     *  The negotiated price of a product.
+     *
+     * @property {string} currency
+     *  The ISO 4217 currency code of the price.
+     */
+
+    /**
+     * A product field.
+     * @typedef {object} CustomField
+     *
+     * @property {string} name
+     *  The name of the custom field.
+     *
+     * @property {string} value
+     *  The value of the custom field.
+     */
+
+    /**
+     * An iterable Field for display.
+     * @typedef {CustomField} IterableField
+     *
+     * @property {number} id
+     *  A unique identifier for the field.
+     */
+
+    /**
+     * Gets or sets which custom fields should be displayed (if supplied).
+     *
+     * @type {CustomField[]}
+     */
+    @api customFields;
+
+    /**
+     * Gets or sets whether the cart is locked
+     *
+     * @type {boolean}
+     */
+    @api cartLocked;
+
+    /**
+     * Gets or sets the name of the product.
+     *
+     * @type {string}
+     */
+    @api description;
+
+    /**
+     * Gets or sets the product image.
+     *
+     * @type {Image}
+     */
+    @api image;
+
+    /**
+     * Gets or sets whether the product is "in stock."
+     *
+     * @type {boolean}
+     */
+    @api inStock = false;
+
+    /**
+     * Gets or sets the name of the product.
+     *
+     * @type {string}
+     */
+    @api name;
+
+    /**
+     * Gets or sets the price - if known - of the product.
+     * If this property is specified as undefined, the price is shown as being unavailable.
+     *
+     * @type {Price}
+     */
+    @api price;
+
+    _invalidQuantity = false;
+    _quantityFieldValue = 1;
+    _categoryPath;
+    _resolvedCategoryPath = [];
+
+    // A bit of coordination logic so that we can resolve product URLs after the component is connected to the DOM,
+    // which the NavigationMixin implicitly requires to function properly.
+    _resolveConnected;
+    _connected = new Promise((resolve) => {
+        this._resolveConnected = resolve;
+    });
+
+    connectedCallback() {
+        this._resolveConnected();
+    }
+
+    disconnectedCallback() {
+        this._connected = new Promise((resolve) => {
+            this._resolveConnected = resolve;
+        });
+    }
+
+    /**
+     * Gets or sets the ordered hierarchy of categories to which the product belongs, ordered from least to most specific.
+     *
+     * @type {Category[]}
+     */
+    @api
+    get categoryPath() {
+        return this._categoryPath;
+    }
+
+    set categoryPath(newPath) {
+        this._categoryPath = newPath;
+        this.resolveCategoryPath(newPath || []);
+    }
+
+    /**
+     * Getter that indicates that product has a price
+     *
+     * @type {Category[]}
+     */
+    get hasPrice() {
+        return ((this.price || {}).negotiated || '').length > 0;
+    }
+
+    /**
+     * Updates the breadcrumb path for the product, resolving the categories to URLs for use as breadcrumbs.
+     *
+     * @param {Category[]} newPath
+     *  The new category "path" for the product.
+     */
+    resolveCategoryPath(newPath) {
+        const path = [homePage].concat(
+            newPath.map((level) => ({
+                name: level.name,
+                type: 'standard__recordPage',
+                attributes: {
+                    actionName: 'view',
+                    recordId: level.id
+                }
+            }))
+        );
+
+        this._connected
+            .then(() => {
+                const levelsResolved = path.map((level) =>
+                    this[NavigationMixin.GenerateUrl]({
+                        type: level.type,
+                        attributes: level.attributes
+                    }).then((url) => ({
+                        name: level.name,
+                        url: url
+                    }))
+                );
+
+                return Promise.all(levelsResolved);
+            })
+            .then((levels) => {
+                this._resolvedCategoryPath = levels;
+            });
+    }
+
+    clickedRegisterLabel = 'REGISTER';
+    showRegVisible = false;
+
+    clickedShowLabel = 'SHOW MORE';
+    showVisible = false;
+
+    /**
+     * handles show button
+     *
+     */
+    handleShowClick(event) {
+        const labelShow = event.target.label;
+
+        if (labelShow === 'SHOW MORE') {
+            this.clickedShowLabel = 'HIDE';
+            this.showVisible = true;
+        } else if (labelShow === 'HIDE') {
+            this.clickedShowLabel = 'SHOW MORE';
+            this.showVisible = false;
+        }
+    }
+
+    /**
+     * Getter of selected course offering
+     *
+     * @type {Object}
+     */
+    get selectedCourseOffering() {
+        let foundCourseOffering = this.courseOfferings.find(key => key.Id === this.selectedOfferingId);
+        return foundCourseOffering ? foundCourseOffering : {};
+    }
+
+    /**
+     * Indicates that user selected a course offering/date
+     *
+     * @type {Boolean}}
+     */
+    get hasSelectedCourseOffering() {
+        return this.selectedCourseOffering.Id ? true : false;
+    }
+
+    /**
+     * Shows the enroll/register button
+     *
+     * @type {Boolean}}
+     */
+    get showEnrollButton() {
+        return this.selectedCourseOffering.Available_Seats__c > 0 ? true : false;
+    }
+
+    /**
+     * Indicates that product has no related couse offering
+     *
+     * @type {Boolean}}
+     */
+    get hasNoRelatedCourseOfferings() {
+        return this.pickOptions.length === 0;
+    }
+
+    /**
+     * Gets the prices and st
+     *
+     * @type {Object}}
+     */
+    get prices() {
+        let pricesObj = [];
+        if (this.priceBookEntries && this.priceBookEntries.length > 0) {
+            pricesObj = this.priceBookEntries.filter((filterKey) => filterKey.UnitPrice).map(key => {
+                return {
+                    priceBookName: key.Pricebook2.Name,
+                    unitPrice: parseInt(key.UnitPrice).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+                }
+            });
+            return pricesObj;
+        }
+        return pricesObj;
+    }
+
+    get optionsPlaceholder() {
+        return this.hasNoRelatedCourseOfferings ? 'NOT AVAILABLE' : 'SELECT A DATE';
+    }
+
+    get pickOptions() {
+        if (this.courseOfferings && this.courseOfferings.length > 0) {
+            let options = this.courseOfferings.filter((filterKey) => filterKey.hed__Start_Date__c).map(key => {
+                return {
+                    label: this.ordinal(new Date(key.hed__Start_Date__c).toLocaleDateString('en-US', { day: 'numeric' }))
+                        + ' ' + new Date(key.hed__Start_Date__c).toLocaleDateString('en-US', { month: 'long' })
+                        + ' ' + new Date(key.hed__Start_Date__c).toLocaleDateString('en-US', { year: 'numeric' }),
+                    value: key.Id
+                }
+            });
+            return options;
+        } else {
+            return [
+            ];
+        }
+    }
+
+    ordinal(day) {
+        var s = ["th", "st", "nd", "rd"];
+        var v = day % 100;
+        return day + (s[(v - 20) % 10] || s[v] || s[0]);
+    }
+
+    handlePickChange(event) {
+        this.selectedDate = event.target.options.find(opt => opt.value === event.detail.value).label;
+        this.selectedOfferingId = event.detail.value;
+    }
+}
