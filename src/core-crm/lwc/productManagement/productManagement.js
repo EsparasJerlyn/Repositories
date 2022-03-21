3f052a7fdd5bcf191d225f086184d15a847e1980
Arsenio Dayrit <arsenio.dayrit@qut.edu.au>
2022-03-21 22:57:57 +0000

    Merged in feature/IT1Feb/JIRA-DEPP-1947 (pull request #996)
    Feature/IT1Feb/JIRA DEPP-1947
    
    * DEPP-1947 created mark content as complete field, validations for course, content section  meta data, and modified admin, qut readonly permset and admin profile
    
    * Merge branch 'release/IT1Feb' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1947
    
    * DEPP-1971
    
    * Merge branch 'release/IT1Feb' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1947
    
    * Merge branch 'release/IT1Feb' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1947
    
    * DEPP-1947
    
    * Merge branch 'release/IT1Feb' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1947
    
    * DEPP-1947
    
    
    Approved-by: Wushi Li

--- src/core-crm/lwc/productManagement/productManagement.js
+++ src/core-crm/lwc/productManagement/productManagement.js
@@ -7,9 +7,13 @@
  *    | Developer                 | Date                  | JIRA                | Change Summary                                         |
       |---------------------------|-----------------------|---------------------|--------------------------------------------------------|
       | angelika.j.s.galang       | February 3, 2022      | DEPP-1257           | Created file                                           |
-      |                           |                       |                     |                                                        |
+      | arsenio.jr.dayrit         | February 14, 2021     | DEPP-1947           | Added Content Section                                                       |
 */
 import { LightningElement, api, wire} from 'lwc';
+import{getRecord, getFieldValue} from "lightning/uiRecordApi";
+import RECORD_TYPE from '@salesforce/schema/Product_Request__c.RecordType.DeveloperName';
+import PR_STATUS from '@salesforce/schema/Product_Request__c.Product_Request_Status__c';
+import RT_ProductRequest_Program from '@salesforce/label/c.RT_ProductRequest_Program';
 import HAS_PERMISSION from '@salesforce/customPermission/EditDesignAndReleaseTabsOfProductRequest';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import LWC_Error_General from '@salesforce/label/c.LWC_Error_General';
@@ -53,6 +57,29 @@ export default class ProductManagement extends LightningElement {
             variant: _variant,
         });
         this.dispatchEvent(evt);
+    }
+    isStatusCompleted
+    isProgram;
+    @wire(getRecord, {recordId: "$recordId",fields: [RECORD_TYPE,PR_STATUS]})
+    productRequestRecordResult(result)
+    {
+        if(result.data){
+            this.isProgram =  getFieldValue(result.data, RECORD_TYPE);
+            this.isStatusCompleted =  getFieldValue(result.data, PR_STATUS);
+        }else if(result.error){
+            this.generateToast('Error.',LWC_Error_General,'error');
+        }
+    }
+
+    isProgramRt = true;
+    isCompleted = true;
+    get HideContentSection(){
+        
+           if(this.isProgram === RT_ProductRequest_Program){
+               return this.isProgramRt;
+           }else if(this.isStatusCompleted === 'Completed'){
+               return this.isCompleted;
+           }
     }
     
 }
