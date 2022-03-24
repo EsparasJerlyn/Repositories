e9a1babebd5a92de9fb9bc7e3f2fec9a72bc62dd
Eccarius Munoz <eccarius.munoz@qut.edu.au>
2022-03-24 12:16:36 +0000

    Merged in feature/IT1Feb/JIRA-DEPP-1888 (pull request #1012)
    DEPP-1888 and DEPP-2111 LWC for Not Proceeding
    
    * Merge branch 'feature/IT1Feb/JIRA-DEPP-2111' of bitbucket.org:qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1888
    
    * DEPP-1888 Updated Sharing Setting and LWC for Not Proceeding
    
    * Merge branch 'release/IT1Feb' of bitbucket.org:qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1888
    
    * Merge branch 'release/IT1Feb' of bitbucket.org:qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1888
    
    * Merge branch 'release/IT1Feb' of bitbucket.org:qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1888
    
    * DEPP-1888 Updated LWC for Not Proceeding
    
    * DEPP-1888 Updated Flexipage for Not Proceeding
    
    * DEPP-1888 Updated LWC and Test Class
    
    * Merge branch 'release/IT1Feb' of bitbucket.org:qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-1888
    
    * DEPP-1888 Permission Set and Updated LWC
    
    
    Approved-by: Wushi Li

--- src/core-crm/lwc/productRequestList/productRequestList.js
+++ src/core-crm/lwc/productRequestList/productRequestList.js
@@ -12,6 +12,7 @@
       | roy.nino.s.regala         | October 01,2021       | DEPP-40,42           | Updated to work with addProductRequest|
       | angelika.j.s.galang       | December 17, 2021     | DEPP-1088,1096       | Modified to handle OPE records        | 
       | roy.nino.s.regala         | March 05, 2022        | DEPP-1747            | Updated Parent to child relationship  |
+      | eccarius.karl.munoz       | March 21, 2022        | DEPP-1888            | Modified to handle Not Proceeding     |
  */
 import { LightningElement, wire,api } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
@@ -25,6 +26,7 @@ import LWC_List_OPEChildFilter from '@salesforce/label/c.LWC_List_OPEChildFilter
 import RT_ProductSpecification_OPEProgramSpecification from '@salesforce/label/c.RT_ProductSpecification_OPEProgramSpecification';
 import RT_ProductRequest_Program from '@salesforce/label/c.RT_ProductRequest_Program';
 import getProductRequests from '@salesforce/apex/ProductRequestListCtrl.getProductRequests';
+import updateProdReqToNotProceeding from '@salesforce/apex/ProductRequestListCtrl.updateProdReqToNotProceeding';
 import PS_RECORD_TYPE from '@salesforce/schema/Product_Specification__c.RecordType.DeveloperName';
 import getFieldLayoutSettings from '@salesforce/apex/AddProductRequestCtrl.getFieldLayoutSettings';
 import PRODUCT_REQUEST_OBJECT from '@salesforce/schema/Product_Request__c';
@@ -37,6 +39,12 @@ const PS_OPE_FILTER = LWC_List_OPEParentFilter.split(COMMA);
 const PR_OPE_FILTER = LWC_List_OPEChildFilter.split(COMMA);
 const ACCORDION_SECTION = 'Product Requests';
 const OPE_ACCORDION_SECTION = 'Add Products';
+const NOT_PROCEEDING_BUTTON_NAME = 'Not Proceeding';
+const ADD_CHILD_BUTTON_NAME = 'Add Child';
+const ERROR_TOAST_VARIANT = 'error';
+const SUCCESS_TOAST_VARIANT = 'success';
+const ERROR_TOAST_TITLE = 'Error';
+const SUCCESS_TOAST_TITLE = 'Success!';
 
 export default class ProductRequestList extends LightningElement {
     @api recordId;
@@ -54,6 +62,11 @@ export default class ProductRequestList extends LightningElement {
     isAddExistingOpen = false;
     recordTypeOrderMapOpe;
     recordTypeOrderMapCce;
+    productRequestRowId;
+    rowIsFlexibleProgram;
+    rowRecordType;
+    productSpecStage;
+    rowProdReqStatus;
     
     /**
      * constructs the header of lightning data grid
@@ -63,11 +76,11 @@ export default class ProductRequestList extends LightningElement {
         this.gridColumns = [
             {
                 fieldName: 'idUrl',
-                label: 'Product Request ID',
+                label: 'Product Name',
                 type: 'url',
                 typeAttributes: {
                     label: {
-                        fieldName: 'id'
+                        fieldName: 'productName'
                     }
                 }
             },
@@ -76,8 +89,8 @@ export default class ProductRequestList extends LightningElement {
                 label: 'Record Type',
             },
             {
-                fieldName: 'name',
-                label: 'Product Request Name',
+                fieldName: 'stage',
+                label: 'Stage',
             },
             {  
                 fieldName: 'ownerUrl',
@@ -90,20 +103,18 @@ export default class ProductRequestList extends LightningElement {
                 }
             },
             {
-                type: 'button',
-                typeAttributes: {
-                    iconName: 'utility:add',
-                    iconPosition: 'right',
-                    label: 'ADD CHILD',
-                    name: 'ADD CHILD',
-                    title: 'ADD CHILD',
-                    disabled: false,
-                    variant: 'brand',
-                    class: {fieldName:'addChildButton'},
+                fieldName: 'notProceedingComments',
+                label: 'Not Proceeding Comments'
+            },
+            { 
+                type: 'action', 
+                typeAttributes: {  
+                    rowActions: this.getRowActions
                 }
-            }     
+            }   
         ];
     }
+
     /**
      * sets name of accordion header
      */
@@ -132,6 +143,9 @@ export default class ProductRequestList extends LightningElement {
         if(result.data){
             this.productRequests = result;
             let parentProductRequests = this.formatData(this.productRequests.data.parentList); //all product requests that has a child
+            if(this.productRequests.data.parentList.length != 0){
+                this.productSpecStage = result.data.parentList[0].Product_Specification__r.Stage__c;
+            }           
             let parentChildProductRequests = this.productRequests.data.parentChildMap; // map of product request to its children
             parentProductRequests.forEach(parentProdReq =>{
                 let childProdReqs = 
@@ -144,7 +158,7 @@ export default class ProductRequestList extends LightningElement {
                 this.gridData = [parentProdReq, ...this.gridData];
             });
         }else if(result.error){
-            this.generateToast('Error.',LWC_Error_General,'error');
+            this.generateToast(ERROR_TOAST_TITLE, LWC_Error_General, ERROR_TOAST_VARIANT);
         }
         this.isLoading = false;
     }
@@ -166,7 +180,7 @@ export default class ProductRequestList extends LightningElement {
                 })
                 .catch(error =>{
                     console.log(error);
-                    this.generateToast('Error.',LWC_Error_General,'error');
+                    this.generateToast(ERROR_TOAST_TITLE, LWC_Error_General, ERROR_TOAST_VARIANT);
                 }); 
             }else{
                 getRecordTypes({
@@ -177,11 +191,11 @@ export default class ProductRequestList extends LightningElement {
                 })
                 .catch(error =>{
                     console.log(error);
-                    this.generateToast('Error.',LWC_Error_General,'error');
+                    this.generateToast(ERROR_TOAST_TITLE, LWC_Error_General, ERROR_TOAST_VARIANT);
                 });
             }
         }else if(result.error){
-            this.generateToast('Error.',LWC_Error_General,'error');
+            this.generateToast(ERROR_TOAST_TITLE, LWC_Error_General, ERROR_TOAST_VARIANT);
         }   
     }
 
@@ -217,33 +231,54 @@ export default class ProductRequestList extends LightningElement {
             newItem.id = item.Name;
             newItem.idUrl = '/' + item.Id;
             newItem.recordType = item.RecordType.Name;
-            newItem.name = item.Product_Request_Name__c;
             newItem.owner = item.Owner.Name;
             newItem.ownerUrl = '/' + item.OwnerId;
+            newItem.stage = item.Product_Request_Status__c;
+            newItem.notProceedingComments = item.Not_Proceeding_Comments__c;
+            newItem.productName = 
+                item.Courses__r && item.Courses__r[0] ? 
+                item.Courses__r[0].Name : 
+                item.Program_Plans__r && item.Program_Plans__r[0] ?
+                item.Program_Plans__r[0].Name : 
+                item.Product_Request_Name__c; 
             if(item.Program_Plans__r && item.Program_Plans__r[0] && item.Program_Plans__r[0].Program_Delivery_Structure__c === 'Flexible Program'){
                 newItem.isFlexibleProgram = true;
             }
             if(item.Courses__r && item.Courses__r[0] && item.Courses__r[0].Name){
                 newItem.courseName = item.Courses__r[0].Name;
-            }
-            newItem.addChildButton = 
-                !isChild && 
-                item.RecordType.DeveloperName === RT_ProductRequest_Program 
-                ?  'slds-show': 'slds-hide';
+            }           
             return newItem;
         });
     }
 
     /**
-     * handles action and data when ADD CHILD button is clicked 
+     * handles action and data when ADD CHILD/NOT PROCEEDING button is clicked 
      */
     handleRowAction(event){
+        let buttonName = event.detail.action.name;
         const row = event.detail.row;
-        let filter = this.isProdSpecOPE ? PR_OPE_FILTER : PR_FILTER;
-        let currentChildren = this.productRequests.data.parentChildMap[row.recordId]?this.productRequests.data.parentChildMap[row.recordId]:[];
-        let newRecord = row.isFlexibleProgram?false:true;
-        this.template.querySelector("c-add-product-request").openSelectionModal(newRecord,row,filter,currentChildren,true,this.prodSpecData());
-        
+        if(buttonName === NOT_PROCEEDING_BUTTON_NAME){
+            this.productRequestRowId = row.recordId;
+            this.rowIsFlexibleProgram = row.isFlexibleProgram;
+            this.rowRecordType = row.recordType;
+            this.rowProdReqStatus = row.stage;
+            if(this.productSpecStage!='Design'){
+                this.generateToast(ERROR_TOAST_TITLE, 'Update after Product Specification Design stage is not allowed.', ERROR_TOAST_VARIANT);
+            }else if(this.rowProdReqStatus!='Design' && this.rowProdReqStatus!='Release'){
+                if(this.rowProdReqStatus == 'Not Proceeding'){
+                    this.generateToast(ERROR_TOAST_TITLE, 'Stage is already not proceeding.', ERROR_TOAST_VARIANT);
+                }else{
+                    this.generateToast(ERROR_TOAST_TITLE, 'Update not allowed after release stage.', ERROR_TOAST_VARIANT);
+                }                
+            }else{
+                this.template.querySelector("c-add-not-proceeding-comments").openSelectionModal(row.notProceedingComments);                
+            }            
+        }else if(buttonName === ADD_CHILD_BUTTON_NAME){  
+            let filter = this.isProdSpecOPE ? PR_OPE_FILTER : PR_FILTER;
+            let currentChildren = this.productRequests.data.parentChildMap[row.recordId]?this.productRequests.data.parentChildMap[row.recordId]:[];
+            let newRecord = row.isFlexibleProgram?false:true;
+            this.template.querySelector("c-add-product-request").openSelectionModal(newRecord,row,filter,currentChildren,true,this.prodSpecData());
+        }
     }
 
     prodSpecData(){
@@ -268,6 +303,58 @@ export default class ProductRequestList extends LightningElement {
         this.isLoading = true;
         this.gridData = [];
         refreshApex(this.productRequests);
+    }
+
+    /**
+     * handles Not Proceeding action
+     */
+    handleNotProceeding(event){
+        
+        this.isLoading = true;
+        let notProceedingComments = event.detail;
+        let response;
+        let ifFlexibleProgram = this.rowIsFlexibleProgram? true : false;
+
+        updateProdReqToNotProceeding({
+            id : this.productRequestRowId,
+            recordType : this.rowRecordType,
+            notProceedingComment : notProceedingComments,
+            ifFlexibleProgram : ifFlexibleProgram
+        }).then((result) => {
+            response = result;
+        })
+        .catch((error) => {
+            response = error;
+        })
+        .finally(() => {            
+            if(response === 'Success'){
+                this.generateToast(SUCCESS_TOAST_TITLE, 'Record updated', SUCCESS_TOAST_VARIANT);
+            } else {
+                this.generateToast(ERROR_TOAST_TITLE, LWC_Error_General, ERROR_TOAST_VARIANT);
+            }
+            this.gridData = [];
+            refreshApex(this.productRequests);
+        });        
+    }
+
+    /**
+     * sets the actions availability per row
+     */
+     getRowActions(row, doneCallback) {
+        const actions = [{ 
+            label: NOT_PROCEEDING_BUTTON_NAME, 
+            name: NOT_PROCEEDING_BUTTON_NAME,
+            title: NOT_PROCEEDING_BUTTON_NAME
+        }];
+
+        if(row.recordType === RT_ProductRequest_Program){
+            actions.push({ 
+                label: ADD_CHILD_BUTTON_NAME, 
+                name: ADD_CHILD_BUTTON_NAME,
+                title: ADD_CHILD_BUTTON_NAME
+            });
+        }
+        doneCallback(actions);
     }
 
     /**
