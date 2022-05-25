4c6830db4fce9012063b12af5ec53fa1c4da0779
Dodge Palattao <dodge.palattao@qut.edu.au>
2022-05-25 03:50:34 +0000

    Merged in feature/IT1Feb/DEPP-2070-V4 (pull request #1392)
    DEPP-2070-v4: Group Booking
    
    * commit latest DEPP-2070-v4
    
    
    Approved-by: Mary Grace Li

--- src/core-crm/lwc/groupBookingForm/groupBookingForm.js
+++ src/core-crm/lwc/groupBookingForm/groupBookingForm.js
@@ -10,6 +10,9 @@ import ACCOUNT_ID from '@salesforce/schema/Contact.AccountId';
 import insertContactData from '@salesforce/apex/ProductDetailsCtrl.saveContactData';
 import getContactAccountId from '@salesforce/apex/ProductDetailsCtrl.getContactAccountId';
 import LWC_Error_General from "@salesforce/label/c.LWC_Error_General";
+import EMAIL from "@salesforce/schema/User.Email";
+import FIRSTNAME from "@salesforce/schema/User.FirstName";
+import LASTNAME from "@salesforce/schema/User.LastName";
 
 const SUCCESS_MSG = 'Record successfully updated.';
 const SUCCESS_TITLE = 'Success!';
@@ -22,14 +25,12 @@ export default class GroupBookingForm extends LightningElement {
     @track isModalOpen;
     @api productDetails;
     @api selectedCourseOffering;
-   // @api questionsTemp;
     @api newproductid;
     @track templatePicklist = true;
     @track numberOfParticipants;
     @track productCourseName;
     @track objectAPIName = 'Contact';
-    @api contactId;// 003Bo000000sP1UIAU
- 
+    @api contactId;
     @track minParticipants;
     @track maxParticipants;
     @track listOfdata = [];
@@ -39,7 +40,6 @@ export default class GroupBookingForm extends LightningElement {
     @track logUser= [];
     @track courseData;
     contactActId;
-    //@track questionsTemp = [];
     @track con;
     //registration Response variables
     isRespondQuestions;
@@ -59,11 +59,20 @@ export default class GroupBookingForm extends LightningElement {
     @track isOpenPayment = false;
     
     
-    @wire(getRecord, { recordId: userId, fields: [CONTACT_ID] })
+    @wire(getRecord, { recordId: userId, fields: [CONTACT_ID,EMAIL,FIRSTNAME,LASTNAME] })
     user;
     get contactId() {
       return getFieldValue(this.user.data, CONTACT_ID);
     }
+    get contactEmail() {
+        return getFieldValue(this.user.data, EMAIL);
+      }
+    get firstName() {
+        return getFieldValue(this.user.data, FIRSTNAME);
+    }
+    get lastName() {
+        return getFieldValue(this.user.data, LASTNAME);
+      }
 
 
     openModal() {
@@ -80,7 +89,6 @@ export default class GroupBookingForm extends LightningElement {
         this.items=[];
         this.disableAddBtn = false;
         this.counter = 1;
-        //this.questions = [];
         this.num = 1;
 
     }
@@ -99,57 +107,20 @@ export default class GroupBookingForm extends LightningElement {
         this.productCourseName = this.productDetails.Name;
         this.minParticipants = this.productDetails.Course__r.Minimum_Participants__c;
         this.maxParticipants =  this.productDetails.Course__r.Maximum_Participants__c;
-        console.log('CONTACT ID new: '+ this.contactId);
-        console.log('productDetails: '+ JSON.stringify(this.productDetails));
-        console.log('Course ID: '+ JSON.stringify(this.productDetails.Course__r.Id));
-        console.log('Minimum: '+ JSON.stringify(this.productDetails.Course__r.Minimum_Participants__c));
-        console.log('Maximum: ' + JSON.stringify(this.productDetails.Course__r.Maximum_Participants__c));
-        console.log('selectedCourseOffering: '+ this.selectedCourseOffering);
         
         getContactAccountId({
             connId: this.contactId
           })
             .then((results) => {
-                console.log('Account Id...'); 
+
               if (results.length > 0) {
                     this.actResponseData = results;
-                    this.contactActId = this.actResponseData[0].AccountId;
-                    console.log('Account Id: '+ this.contactActId); 
-                 //console.log('responseData 1: '+ JSON.stringify(this.responseData)); 
-                //console.log('responseData 2: '+ JSON.stringify(this.responseData[0].AccountId));
-               // this.contactActId = JSON.parse(JSON.stringify(this.responseData[0].AccountId));
-                  //alert('===slected record details==='+JSON.stringify(this.responseData[0].AccountId));    
+                    this.contactActId = this.actResponseData[0].AccountId; 
               }
             })
             .catch((e) => {
               this.generateToast("Error.", LWC_Error_General, "error");
             });
-
-
-            // let fields = {};
-            // fields.Id = this.contactId;//003Bo000000sP1UIAU
-            // //fields.Id = "003Bo000000sP1UIAU";//003Bo000000sP1UIAU
-            // console.log('CONTACT fields: '+ this.contactId);
-            // this.contactFields = fields;
-            // if (this.hasQuestions) {
-            //     this.handleRespondQuestions();
-            //     console.log('SHOW Questions');
-            // }   
-            
-            // getCourseDetails({
-            //     courseOffId: this.productDetails.Course__r.Id
-            //   })
-            //     .then((results) => {
-            //       if (results.length > 0) {
-            //             this.courseData = results; 
-            //             console.log("COURSE DETAILS------>" + JSON.stringify(this.courseData));
-            //       }
-            //     })
-            //     .catch((e) => {
-            //       this.generateToast("Error.", LWC_Error_General, "error");
-            //     });                      
-
-
     }
   
     getQuestionsList(){
@@ -159,7 +130,6 @@ export default class GroupBookingForm extends LightningElement {
      // This handle the picklist for number of participants
     async handleAfterPick(event){
 
-        console.log('ACT ID: '+ this.contactActId);
         getQuestionsForGroupBooking({
             productReqId: this.productDetails.Course__r.ProductRequestID__c
          
@@ -167,21 +137,13 @@ export default class GroupBookingForm extends LightningElement {
             .then((results) => {
               if (results.length > 0) {
                     this.responseData2 = results;
-                    
-                    //this.questions2 = this.formatQuestions(results);
                     this.questions2= this.formatQuestions(results);
                     this.questionsPrimary= this.formatQuestions(results);
-                   console.log('question array: '+ JSON.stringify(this.questions2));  
-                   // console.log('responseData: '+ JSON.stringify(this.responseData.LastName)); 
-                   // console.log('questionsTemp: '+ JSON.stringify(this.questionsTemp));
-  
               }
             })
             .catch((e) => {
               this.generateToast("Error.", LWC_Error_General, "error");
             });
-
-
         this.numberOfParticipants = event.detail.value;
         if(this.numberOfParticipants == this.counter){
             this.disableAddBtn = true;
@@ -194,17 +156,14 @@ export default class GroupBookingForm extends LightningElement {
     //This handle the change on accordion data
     updateOnAccordionDetails(event) {
         this.items[event.currentTarget.dataset.id][event.target.name] = event.target.value;
-      //  this.info = JSON.stringify(this.items);
-       // console.log('LIST OF CONTACTS: ' + this.info);
     }
     //This handle added participants
     addParticipant() {
-
         this.currentIndex = this.currentIndex + 1;   
         //Contact list
         this.items = [...this.items, 
             { 
-                id: this.items.length, //key
+                id: this.items.length, 
                 FirstName: '', 
                 Email: '',
                 Birthdate: '',
@@ -214,16 +173,9 @@ export default class GroupBookingForm extends LightningElement {
                 label: 'PARTICIPANT ' + this.currentIndex, 
                 Questions: this.questions2
             }
-            ];
-        console.log('Current Index: ' + this.currentIndex);    
+            ];  
         this.info = JSON.stringify(this.items);
-        console.log('LIST: ' + this.info);
-        //this.info = JSON.stringify(this.questions2);
-        //console.log('questions array: ' + this.info);
         this.counter++;
-        console.log('COUNTER: ' + this.counter , this.numberOfParticipants);
-        //console.log("Product Details ------->: " + JSON.stringify(this.productDetails));
-        
 
         if(this.counter < this.numberOfParticipants){
             this.disableAddBtn = false;
@@ -236,23 +188,16 @@ export default class GroupBookingForm extends LightningElement {
     handleClick(event){
     
         if(event.target.name === 'openConfirmation'){
-            //it can be set dynamically based on your logic
-            //this.originalMessage = 'test message';
-            //shows the component
-
-            this.isDialogVisible = true;
+              //shows the component
+             this.isDialogVisible = true;
 
         }else if(event.target.name === 'confirmModal'){
-
             //when user clicks outside of the dialog area, the event is dispatched with detail value  as 1
             if(event.detail !== 1){
-                //gets the detail message published by the child component
-                //this.displayMessage = 'Status: ' + event.detail.status + '. Event detail: ' + JSON.stringify(event.detail.originalMessage) + '.';
                
                 if(event.detail.status === 'confirm') {
 
                     this.items = this.items.filter(function (element) {
-                        console.log('=====' + parseInt(event.target.accessKey));
                         return parseInt(element.id) !== parseInt(event.target.accessKey);
                     });
                     
@@ -265,13 +210,10 @@ export default class GroupBookingForm extends LightningElement {
                     else{
                         this.disableAddBtn = true;
                     }
-                    console.log('----' + JSON.stringify(this.items));
                 }
-                else if(event.detail.status === 'cancel'){
-                   
+                else if(event.detail.status === 'cancel'){         
                 }
             }
-
             //hides the component
             this.isDialogVisible = false;
         }
@@ -311,10 +253,8 @@ export default class GroupBookingForm extends LightningElement {
             newItem.FileData = undefined;
             return newItem;
         });
-      
         return questions2;
        }
-
        handleRespondQuestions(){
         this.isRespondQuestions = true;
       }
@@ -323,12 +263,9 @@ export default class GroupBookingForm extends LightningElement {
         return this.questions && this.questions.length > 0?true:false;
    }
 
-
    submitDetails(event) {
     // to close modal set isModalOpen tarck value as false
     //Add your code to call apex method or do some processing
-    // this.saveInProgress = true;
-    console.log("test 001:"+ JSON.stringify(this.items));
     //save answer for Primary contact
     let fieldsPrimary = {};
     fieldsPrimary.Id = this.contactId;
@@ -340,27 +277,20 @@ export default class GroupBookingForm extends LightningElement {
         for(let i = 0; i < blankRow.length; i++){
             if(blankRow[i] !== undefined){
                 let conData = new Object();
-                //console.log("account id2");
-               // console.log('for save line 1: ' + blankRow[i].FirstName);
-               // console.log('for save line: ' + JSON.stringify(blankRow[i].FirstName));
-                conData.AccountId =  this.contactActId; 
                 conData.FirstName = blankRow[i].FirstName;
                 conData.LastName = blankRow[i].LastName;
                 conData.Email = blankRow[i].Email;
                 conData.Birthdate = blankRow[i].Birthdate;
                 conData.MobilePhone = blankRow[i].MobilePhone;
-                conData.Dietary_Requirement__c = blankRow[i].Dietary_Requirement__c;
-                
+                conData.Dietary_Requirement__c = blankRow[i].Dietary_Requirement__c;    
                 contactDataList.push(conData);
             }
         }
         if(contactDataList.length > 0){
-            console.log('processing insert..');
             insertContactData({contactDataString: JSON.stringify(contactDataList)}).then(result => {
                 for(let i = 0; i < result.length; i++){
                     if(result[i] !== undefined){
                         let contactRecord = {'sobjectType' : 'Contact'};
-                        console.log('insert...done');
                         contactRecord.Id = result[i].Id; //newly created contact ID
                         contactRecord.FirstName = result[i].FirstName;
                         contactRecord.LastName = result[i].LastName;
@@ -372,15 +302,11 @@ export default class GroupBookingForm extends LightningElement {
 
                         let ItemsRecords = {};
                         ItemsRecords = this.items.map(item=>{  
-                            console.log("item.Id: "+JSON.stringify(item.id)); 
-                            console.log("blankRow Id: "+blankRow[i].id); 
-                    
+
                             if (blankRow[i].id == item.id){
-                                console.log("test 1b");
 
                                 let answerRecords = {};
                                 answerRecords = item.Questions.map(row=>{
-                                    console.log("test 1b -> row.Id: "+row.Id); 
                                     let record = {};
                                     record.Related_Answer__c = row.Id;
                                     record.Response__c = row.Answer;
@@ -388,35 +314,22 @@ export default class GroupBookingForm extends LightningElement {
                                     return record;                                  
                                 });
                                 this.answerRecords2 =  answerRecords;
-                                console.log('answerRecords2 [' + i +'] ->'+JSON.stringify(this.answerRecords2));
+                            
                             }
                             return item;
                         });
 
-                        this.saveRegistration(this.contactFields, this.selectedCourseOffering,this.responseData2, this.answerRecords2 ,JSON.stringify(this.createFileUploadMap()));                      
-                        console.log("account id from database: "+JSON.stringify(result[i].AccountId));
-                        console.log("contact id from database: "+JSON.stringify(result[i].Id));
-                        console.log("contactFields data: "+JSON.stringify(this.contactFields));
-                        console.log("selectedCourseOffering data: "+JSON.stringify(this.selectedCourseOffering));
-                        console.log("reponse data: "+JSON.stringify(this.responseData2));
-                       // console.log("Answer data: "+JSON.stringify(this.createAnswerRecord()));
-                       
+                        this.saveRegistration(this.contactFields, this.selectedCourseOffering,this.responseData2, this.answerRecords2 ,JSON.stringify(this.createFileUploadMap()));
+
                     }
                     this.generateToast(SUCCESS_TITLE, 'Successfully Submitted', SUCCESS_VARIANT);
-                    console.log('Submitted');
                     this.isOpenPayment = true;
                     
                 }
             }).catch(error => {
-                // window.alert('Please contact system admin: ' + error);
-                console.log("event.target.contactId: "+JSON.stringify(error));
             })
         }else{
-            //window.alert('Please select any row to insert data.');
         }
-        
-        // this.saveInProgress = false;
-  // this.isModalOpen = false;
 }
 
 createFileUploadMap(){
@@ -448,7 +361,6 @@ createAnswerRecord(){
 
   }
 
-
 saveRegistration(contact,courseOffering,relatedAnswer,answer,fileUpload){
     addRegistration({
         contactRecord:contact,
@@ -463,18 +375,9 @@ saveRegistration(contact,courseOffering,relatedAnswer,answer,fileUpload){
             
     })
     .finally(()=>{
-        // this.saveInProgress = false;
-        // this.isModalOpen = false;
-        // this.isEditContact = false;
-        // this.isAddContact = false;
-        // this.isCreateContact = false;
-        // this.isLoading = false;
-        // this.saveInProgress = false;
-        // this.contactId2 = '';
-        // this.contactSearchItems = [];
+
     })
     .catch(error =>{
-       // this.generateToast('Error.',LWC_Error_General,'error');
        
     });
   }
@@ -493,26 +396,11 @@ saveRegistration(contact,courseOffering,relatedAnswer,answer,fileUpload){
     this.dispatchEvent(evt);
   }
 
-
   handleChange(event){
-    console.log("handleChange dataset 1: "+JSON.stringify(event.target.dataset.contactId));  
-    console.log("handleChange dataset 2: "+JSON.stringify(event.target.dataset.questionId));  
-    console.log("event.target.questionId: "+JSON.stringify(event.target.questionId)); 
-    console.log("event.target.contactId: "+JSON.stringify(event.target.contactId)); 
-    console.log('UPDATE ITEMS 1: ' + JSON.stringify( event.target.value));  
-    console.log('UPDATE ITEMS 2: ' + JSON.stringify(this.items));      
- 
     this.items = this.items.map(item=>{  
-        console.log("item.Id: "+JSON.stringify(item.id)); 
-        console.log("contactId: "+event.target.dataset.contactId); 
-
         if (event.target.dataset.contactId == item.id){
-            console.log("test 1");
 
             Questions = item.Questions.map(row=>{
-                console.log("row.Id: "+row.Id); 
-                console.log("question.Id: "+event.target.dataset.questionId); 
-
                 if(event.target.dataset.questionId === row.Id && row.IsCheckbox){ //checkbox
                     row.Answer = event.detail.checked.toString();
                 }else if(event.target.dataset.questionId === row.Id && row.IsFileUpload){  //fileupload
@@ -542,22 +430,16 @@ saveRegistration(contact,courseOffering,relatedAnswer,answer,fileUpload){
 
   }
   
-  handleBlur(event){
-    //   console.log("handleBlur detail: "+JSON.stringify(event.detail));
-    //   console.log("handleBlur target: "+JSON.stringify(event.target));  
-    //   console.log("handleBlur event: "+JSON.stringify(event));
-    //   console.log("handleBlur dataset 1: "+JSON.stringify(event.target.dataset.contactId));  
-    //   console.log("handleBlur dataset 2: "+JSON.stringify(event.target.dataset.questionId));                  
-    // this.questions2 = this.questions2.map(row=>{
-    //     if(row.IsCriteria && row.Answer!= '' && row.Answer.toUpperCase() != row.MandatoryResponse.toUpperCase()){
-    //         row.Answer = '';
-    //         row.ErrorMessage = row.Message?row.Message:'You are not qualified to proceed with registration.';
-    //     }else if(row.IsCriteria && row.Answer!= '' && row.Answer.toUpperCase() == row.MandatoryResponse.toUpperCase()){
-    //         row.ErrorMessage = '';
-    //     }
-    //     return row;
-    // });
-
+  handleBlur(event){                  
+    this.items.Questions = this.items.Questions.map(row=>{
+        if(row.IsCriteria && row.Answer!= '' && row.Answer.toUpperCase() != row.MandatoryResponse.toUpperCase()){
+            row.Answer = '';
+            row.ErrorMessage = row.Message?row.Message:'You are not qualified to proceed with registration.';
+        }else if(row.IsCriteria && row.Answer!= '' && row.Answer.toUpperCase() == row.MandatoryResponse.toUpperCase()){
+            row.ErrorMessage = '';
+        }
+        return row;
+    });
   }  
 
   handleChangePrimary(event){
@@ -597,8 +479,7 @@ saveRegistration(contact,courseOffering,relatedAnswer,answer,fileUpload){
         return row;
     });
   }
-
-
+  
   createAnswerRecordPrimary(){
     let answerRecords = {};
     answerRecords = this.questionsPrimary.map(item =>{
@@ -610,7 +491,5 @@ saveRegistration(contact,courseOffering,relatedAnswer,answer,fileUpload){
     });
     return answerRecords;
   }
-
-
 
 }
