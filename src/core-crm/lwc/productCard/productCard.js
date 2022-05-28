129e7031efe6396d800b8b5ec322b371b6674492
John Bo Pineda <johnbo.pineda@qut.edu.au>
2022-05-28 11:16:43 +0000

    Merged in feature/IT1Feb/DEPP-2692 (pull request #1413)
    DEPP-2692: Preview Button - Placement of button and its functionality
    
    * DEPP-2692: Preview Button - Placement of button and its functionality
    
    
    Approved-by: Mary Grace Li

--- src/core-crm/lwc/productCard/productCard.js
+++ src/core-crm/lwc/productCard/productCard.js
@@ -8,51 +8,58 @@
       | keno.domienri.dico        | April 29, 2022        | DEPP-2038            | Create child product display records                  |
  */
 
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 import qutResourceImg from "@salesforce/resourceUrl/QUTImages";
-import { NavigationMixin } from 'lightning/navigation';
+import { NavigationMixin } from "lightning/navigation";
 /**
  * An organized display of a single product card.
  *
  * @fires ProductCard#showdetail
  */
 export default class ProductCard extends NavigationMixin(LightningElement) {
-    @api productDetails;
-    // Icons
-    clipboard_icon;
-    dollar_icon;
-    location_icon;
-    time_icon;
-    // URL 
-    recordPageUrl;
-    urlString;
-    currentURL;
-    productPath;
-    baseUrl;
-    cProductId;
-    cProductName;
+  @api productDetails;
+  @api readOnly = false;
+  // Icons
+  clipboard_icon;
+  dollar_icon;
+  location_icon;
+  time_icon;
+  // URL
+  recordPageUrl;
+  urlString;
+  currentURL;
+  productPath;
+  baseUrl;
+  cProductId;
+  cProductName;
 
-    // Navigate to the Single Product Page
-    navigateToProductPage(event) {
-        this.cProductId = event.target.dataset.id;
-        this.cProductName = event.target.dataset.name;
-        this.productPath = window.location.pathname;
-        this.baseUrl = window.location.origin;
-        this.recordPageUrl = this.baseUrl + '/study/s/product/' + this.cProductName.replaceAll(' ','-') + '/' + this.cProductId;
-        console.log('URL: ' + this.recordPageUrl);
+  // Navigate to the Single Product Page
+  navigateToProductPage(event) {
+    if (!this.readOnly) {
+      this.cProductId = event.target.dataset.id;
+      this.cProductName = event.target.dataset.name;
+      this.productPath = window.location.pathname;
+      this.baseUrl = window.location.origin;
+      this.recordPageUrl =
+        this.baseUrl +
+        "/study/s/product/" +
+        this.cProductName.replaceAll(" ", "-") +
+        "/" +
+        this.cProductId;
+      console.log("URL: " + this.recordPageUrl);
     }
+  }
 
-    connectedCallback() {
-        // Get icons
-        this.getIcons();        
-    }
+  connectedCallback() {
+    // Get icons
+    this.getIcons();
+  }
 
-    // Get Icons from Static Resources
-    getIcons(){
-        this.clipboard_icon = qutResourceImg + "/QUTImages/Icon/clipboard_icon.svg";
-        this.dollar_icon = qutResourceImg + "/QUTImages/Icon/dollar_icon.svg";
-        this.time_icon = qutResourceImg + "/QUTImages/Icon/time_icon.svg";
-        this.location_icon = qutResourceImg + "/QUTImages/Icon/location_icon.svg";
-    }
-    
+  // Get Icons from Static Resources
+  getIcons() {
+    this.clipboard_icon = qutResourceImg + "/QUTImages/Icon/clipboard_icon.svg";
+    this.dollar_icon = qutResourceImg + "/QUTImages/Icon/dollar_icon.svg";
+    this.time_icon = qutResourceImg + "/QUTImages/Icon/time_icon.svg";
+    this.location_icon = qutResourceImg + "/QUTImages/Icon/location_icon.svg";
+  }
 }
--- src/core-crm/lwc/productDetailsDisplayInternal/productDetailsDisplayInternal.js
+++ src/core-crm/lwc/productDetailsDisplayInternal/productDetailsDisplayInternal.js
@@ -6,7 +6,6 @@ import facilitator from "@salesforce/label/c.QUT_ProductDetail_Facilitator";
 import evolveWithQUTeX from "@salesforce/label/c.QUT_ProductDetail_EvolveWithQUTeX";
 import whoShouldParticipate from "@salesforce/label/c.QUT_ProductDetail_WhoShouldParticipate";
 import coreConcepts from "@salesforce/label/c.QUT_ProductDetail_CoreConcepts";
-import location from "@salesforce/label/c.QUT_ProductDetail_Location";
 import details from "@salesforce/label/c.QUT_ProductDetail_Details";
 import delivery from "@salesforce/label/c.QUT_ProductDetail_Delivery";
 import deliveryPlaceholder from "@salesforce/label/c.QUT_ProductDetail_Delivery_Placeholder";
@@ -36,7 +35,6 @@ export default class ProductDetailsDisplay extends LightningElement {
   @track displayFacilitatorNav = true;
   @track facilitatorIndex = 0;
   @track selectedCourseOfferingFacilitator = [];
-  @track selectedCourseOfferingLocation;
 
   disableDelivery;
   @track courseOfferings = [];
@@ -45,6 +43,7 @@ export default class ProductDetailsDisplay extends LightningElement {
   @track disableAvailStartDate = true;
   @track disablePriceBookEntry = true;
   @track disableAddToCart = true;
+  @track selectedDelivery;
 
   label = {
     overview,
@@ -54,7 +53,6 @@ export default class ProductDetailsDisplay extends LightningElement {
     evolveWithQUTeX,
     whoShouldParticipate,
     coreConcepts,
-    location,
     details,
     delivery,
     deliveryPlaceholder,
@@ -63,141 +61,142 @@ export default class ProductDetailsDisplay extends LightningElement {
     pricing,
     pricingPlaceholder,
     addToCart
-  }
+  };
 
   renderedCallback() {
     Promise.all([loadStyle(this, customSR + "/qutCustomLwcCss.css")]);
   }
 
-  connectedCallback(){
-    console.log('this.product: '+JSON.stringify(this.product));
+  connectedCallback() {
+    console.log("this.product: " + JSON.stringify(this.product));
     this.productDetails = this.product.productDetails;
-    this.priceBookEntryList = this.product.priceBookEntryList;
+    this.priceBookEntries = this.product.priceBookEntryList;
     this.deliveryOptions = this.product.deliveryOptions;
     this.cProducts = this.product.cProducts;
     this.hasChildProducts = this.cProducts.length == 0 ? false : true;
     this.isNotFlexProgram = this.product.isNotFlexProgram;
     this.accordionIcon = qutResourceImg + "/QUTImages/Icon/accordionClose.svg";
     this.durationIcon = qutResourceImg + "/QUTImages/Icon/duration.svg";
-    this.pdmStaticText =  "Each " + this.productDetails.Name + "Professional Development Module is a stand-alone course. Choose the expertise you need and explore each topic to find the time frame that suits you.";
+    this.pdmStaticText =
+      "Each " +
+      this.productDetails.Name +
+      "Professional Development Module is a stand-alone course. Choose the expertise you need and explore each topic to find the time frame that suits you.";
     this.disableDelivery = this.deliveryOptions.length == 0 ? true : false;
   }
 
-    handleAccordionToggle(event) {
-      let accordionAriaExpanded = event.currentTarget;
-      let accordionSection = event.currentTarget.closest("section");
-      let accordionContent = accordionSection.querySelector(".accordionContent");
-      let accordionIcon = accordionSection.querySelector(".slds-button__icon");
-      accordionSection.classList.toggle("slds-is-open");
-      if (accordionAriaExpanded.getAttribute("aria-expanded") == "true") {
-        accordionAriaExpanded.setAttribute("aria-expanded", "false");
-        accordionContent.setAttribute("hidden");
-        accordionIcon.setAttribute(
-          "src",
-          qutResourceImg + "/QUTImages/Icon/accordionClose.svg"
-        );
-      } else {
-        accordionAriaExpanded.setAttribute("aria-expanded", "true");
-        accordionContent.removeAttribute("hidden");
-        accordionIcon.setAttribute(
-          "src",
-          qutResourceImg + "/QUTImages/Icon/accordionOpen.svg"
-        );
-      }
+  handleAccordionToggle(event) {
+    let accordionAriaExpanded = event.currentTarget;
+    let accordionSection = event.currentTarget.closest("section");
+    let accordionContent = accordionSection.querySelector(".accordionContent");
+    let accordionIcon = accordionSection.querySelector(".slds-button__icon");
+    accordionSection.classList.toggle("slds-is-open");
+    if (accordionAriaExpanded.getAttribute("aria-expanded") == "true") {
+      accordionAriaExpanded.setAttribute("aria-expanded", "false");
+      accordionContent.setAttribute("hidden");
+      accordionIcon.setAttribute(
+        "src",
+        qutResourceImg + "/QUTImages/Icon/accordionClose.svg"
+      );
+    } else {
+      accordionAriaExpanded.setAttribute("aria-expanded", "true");
+      accordionContent.removeAttribute("hidden");
+      accordionIcon.setAttribute(
+        "src",
+        qutResourceImg + "/QUTImages/Icon/accordionOpen.svg"
+      );
     }
+  }
 
-    handlePreviousFacilitator() {
-      if (this.facilitatorIndex == 0) {
-        this.facilitatorIndex = this.selectedCourseOfferingFacilitator.length - 1;
-      } else {
-        this.facilitatorIndex--;
-      }
-      this.facilitator = this.selectedCourseOfferingFacilitator[this.facilitatorIndex];
+  handlePreviousFacilitator() {
+    if (this.facilitatorIndex == 0) {
+      this.facilitatorIndex = this.selectedCourseOfferingFacilitator.length - 1;
+    } else {
+      this.facilitatorIndex--;
     }
-  
-    handleNextFacilitator() {
-      if (
-        this.facilitatorIndex ==
-        this.selectedCourseOfferingFacilitator.length - 1
-      ) {
+    this.facilitator =
+      this.selectedCourseOfferingFacilitator[this.facilitatorIndex];
+  }
+
+  handleNextFacilitator() {
+    if (
+      this.facilitatorIndex ==
+      this.selectedCourseOfferingFacilitator.length - 1
+    ) {
+      this.facilitatorIndex = 0;
+    } else {
+      this.facilitatorIndex++;
+    }
+    this.facilitator =
+      this.selectedCourseOfferingFacilitator[this.facilitatorIndex];
+  }
+
+  handleDeliverySelected(event) {
+    this.selectedDelivery = event.detail.value;
+
+    getRelatedCourseOffering({
+      courseId: this.productDetails.Course__c,
+      deliveryParam: this.selectedDelivery
+    })
+      .then((results) => {
+        this.courseOfferings = undefined;
+        this.selectedCourseOffering = undefined;
+        this.selectedCourseOfferingFacilitator = undefined;
+        this.facilitator = undefined;
+        this.displayFacilitatorNav = true;
         this.facilitatorIndex = 0;
-      } else {
-        this.facilitatorIndex++;
-      }
-      this.facilitator = this.selectedCourseOfferingFacilitator[this.facilitatorIndex];
-    }
+        this.selectedPriceBookEntry = undefined;
+        this.disableAvailStartDate = true;
+        this.disablePriceBookEntry = true;
+        this.disableAddToCart = true;
 
-    handleDeliverySelected(event) {
-      if (event.detail) {
-        let yourSelectedValues = [];  
-        event.detail.forEach(function (eachItem) {
-          yourSelectedValues.push(eachItem.value);
-        });  
-        getRelatedCourseOffering({
-          courseId: this.productDetails.Course__c,
-          deliveryParam: yourSelectedValues
-        })
-          .then((results) => {
-            this.courseOfferings = undefined;
-            this.selectedCourseOffering = undefined;
-            this.selectedCourseOfferingLocation = undefined;
-            this.selectedCourseOfferingFacilitator = undefined;
-            this.facilitator = undefined;
-            this.displayFacilitatorNav = true;
-            this.facilitatorIndex = 0;
-            this.selectedPriceBookEntry = undefined;
-            this.disableAvailStartDate = true;
-            this.disablePriceBookEntry = true;
-            this.disableAddToCart = true;
-  
-            if (results.length > 0) {
-              this.courseOfferings = results;
-              this.disableAvailStartDate = false;
-            }
-          })
-          .catch((e) => {
-            this.generateToast("Error.", LWC_Error_General, "error");
-          });
-      }
-    }
+        if (results.length > 0) {
+          this.courseOfferings = results;
+          this.disableAvailStartDate = false;
+        }
+      })
+      .catch((e) => {
+        this.generateToast("Error.", LWC_Error_General, "error");
+      });
+  }
 
-    handleStartDateSelected(event) {
-      this.displayFacilitatorNav = true;
-      this.selectedCourseOffering = event.detail.value;
-      this.courseOfferings.forEach((cOffer) => {
-        if (cOffer.value === this.selectedCourseOffering) {
-          this.selectedCourseOfferingLocation = cOffer.location;
-          this.selectedCourseOfferingFacilitator = cOffer.facilitator;
-          if (this.selectedCourseOfferingFacilitator.length > 0) {
-            this.facilitator = this.selectedCourseOfferingFacilitator[this.facilitatorIndex];
-            if (this.selectedCourseOfferingFacilitator.length == 1) {
-              this.displayFacilitatorNav = false;
-            }
+  handleStartDateSelected(event) {
+    this.displayFacilitatorNav = true;
+    this.selectedCourseOffering = event.detail.value;
+    this.courseOfferings.forEach((cOffer) => {
+      if (cOffer.value === this.selectedCourseOffering) {
+        this.selectedCourseOfferingFacilitator = cOffer.facilitator;
+        if (this.selectedCourseOfferingFacilitator.length > 0) {
+          this.facilitator =
+            this.selectedCourseOfferingFacilitator[this.facilitatorIndex];
+          if (this.selectedCourseOfferingFacilitator.length == 1) {
+            this.displayFacilitatorNav = false;
           }
         }
-      });
-      this.disablePriceBookEntry = false;
-    }
-
-    handlePricebookSelected(event) {
-      this.selectedPriceBookEntry = event.detail.value;
-      if(this.isInternalUser == true){
-        this.disableAddToCart = true;
-      } else{
-        this.disableAddToCart = false;            
       }
-      this.priceBookEntries.forEach((pBookEntry) => {
-        if (pBookEntry.value === this.selectedPriceBookEntry && pBookEntry.label == "Group Booking" ) {
-          this.displayAddToCart = false;
-        } else {
-          this.displayAddToCart = true;
-        }
-      });
-    }
+    });
+    this.disablePriceBookEntry = false;
+  }
 
-    notifyAddToCart() {
-      console.log('Internal User');
+  handlePricebookSelected(event) {
+    this.selectedPriceBookEntry = event.detail.value;
+    if (this.isInternalUser == true) {
+      this.disableAddToCart = true;
+    } else {
+      this.disableAddToCart = false;
     }
-  
-  
+    this.priceBookEntries.forEach((pBookEntry) => {
+      if (
+        pBookEntry.value === this.selectedPriceBookEntry &&
+        pBookEntry.label == "Group Booking"
+      ) {
+        this.displayAddToCart = false;
+      } else {
+        this.displayAddToCart = true;
+      }
+    });
+  }
+
+  notifyAddToCart() {
+    console.log("Internal User");
+  }
 }
