bc056b91d0a83114cd2136f7c86ac8fcfd8d6f9c
John Esquerra <john.esquerra@qut.edu.au>
2022-05-31 23:16:42 +0000

    Merged in feature/IT1Feb/DEPP-2839 (pull request #1450)
    DEPP-2839: header overlapping, bg color
    
    * DEPP-2839: header overlapping, bg color
    
    
    Approved-by: Adam Best

--- src/core-crm/lwc/storeHeader/storeHeader.js
+++ src/core-crm/lwc/storeHeader/storeHeader.js
@@ -3,6 +3,10 @@ import qutResourceImg from "@salesforce/resourceUrl/QUTImages";
 
 export default class StoreHeader extends LightningElement {
     qutexlogoUrl = qutResourceImg + "/QUTImages/Logo/QUT_logo.png";
+    qutMenu= qutResourceImg + "/QUTImages/Icon/icon-menu.svg";	
 
+    connectedCallback(){
+        this.qutMenu= qutResourceImg + "/QUTImages/Icon/icon-menu.svg";	
+    }
     
 }
