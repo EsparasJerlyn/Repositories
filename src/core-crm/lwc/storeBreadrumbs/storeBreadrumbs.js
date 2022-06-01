56932f3ebac91cb537d9dd442370fe30e4479dc4
Dodge Palattao <dodge.palattao@qut.edu.au>
2022-06-01 18:55:46 +0000

    Merged in feature/IT1Feb/DEPP-2776v3 (pull request #1467)
    DEPP-2776/2811(For You link breadcrumbs) & DEPP2809(Product name) bug fix
    
    * Merged release/IT1Feb into feature/IT1Feb/DEPP-2776v3
    * DEPP-2776 For You link bug fix
    
    
    Approved-by: Mary Grace Li

--- src/core-crm/lwc/storeBreadrumbs/storeBreadrumbs.js
+++ src/core-crm/lwc/storeBreadrumbs/storeBreadrumbs.js
@@ -2,42 +2,40 @@ import { LightningElement, wire, api } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import NAME_FIELD from '@salesforce/schema/Product2.Name';
 import qutResourceImg from "@salesforce/resourceUrl/QUTImages";
+import getProducts from '@salesforce/apex/ProductCtrl.getProducts';
 
 export default class StoreBreadrumbs extends LightningElement {
 
     @api
     recordId;
-
-    /**
-     * fetches product data
-     */
-    @wire(getRecord, { recordId:'$recordId', fields: [NAME_FIELD]})
-    product;
-
+    dataId;
+    dataName;
+    forYouLink;
 
     connectedCallback() {
-        //this._resolveConnected();
-        //Promise.all([loadStyle(this, QUTMainCSS + "/QUTMainCSS.css")]);
-        this.iconhome = qutResourceImg + "/QUTImages/Icon/icon-home-blue.svg";	
+        this.iconhome = qutResourceImg + "/QUTImages/Icon/icon-home-blue.svg";
+        let currentUrl = window.location.href;
+        this.recordId = currentUrl.substr(currentUrl.length - 18);
+        let productIds =  [
+            this.recordId
+        ];
+        getProducts({
+            productIds: productIds
+        }).then(result => {
+            if(result.productList.length > 0){
+                this.dataName = result.productList[0].childProdName;
+            }
+        }).catch(
+            // do nothing
+        );
+        this.forYouLink = document.referrer;
     } 
-
-    /**
-     * gets product name
-     */
-     get prodName() {
-        return getFieldValue(this.product.data, NAME_FIELD);
-    }
-
     changeTextDecorNone(event){
         const el = event.target;
         el.setAttribute('style', 'text-decoration:none');
     }
-
     changeTextDecorUnderline(event){
         const el = event.target;
         el.setAttribute('style', 'text-decoration:underline');
     }
-
-     
-
 }
