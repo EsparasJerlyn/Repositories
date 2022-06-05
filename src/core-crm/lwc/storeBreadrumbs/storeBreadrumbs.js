fef8d7cd65f1b8089bebfc7fde325fd19bdb88e2
Dodge Palattao <dodge.palattao@qut.edu.au>
2022-06-05 00:46:32 +0000

    Merged in feature/IT1Feb/JIRA-DEPP-2809 (pull request #1514)
    DEPP-2809 For You link storebreadcrumbs
    
    * ForYou link storebreadcrumbs  -DEPP2809
    
    * DEPP-2809 storebreadcrumbs update
    
    * Merged release/IT1Feb into feature/IT1Feb/JIRA-DEPP-2809
    * Merge branch 'release/IT1Feb' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-2809
    
    * DEPP-2809 modified mainNavigationCntr/mainNavigationCntrTest
    
    * Merge branch 'feature/IT1Feb/JIRA-DEPP-2809' of https://bitbucket.org/qutdev/sfdx into feature/IT1Feb/JIRA-DEPP-2809
    
    
    Approved-by: Adam Best

--- src/core-crm/lwc/storeBreadrumbs/storeBreadrumbs.js
+++ src/core-crm/lwc/storeBreadrumbs/storeBreadrumbs.js
@@ -1,35 +1,64 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire, api,track } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import NAME_FIELD from '@salesforce/schema/Product2.Name';
+import communityId from '@salesforce/community/Id';
+import basePath from '@salesforce/community/basePath';
 import qutResourceImg from "@salesforce/resourceUrl/QUTImages";
+import getStudyProducts from '@salesforce/apex/MainNavigationMenuCtrl.getStudyProducts';
 import getProducts from '@salesforce/apex/ProductCtrl.getProducts';
 
 export default class StoreBreadrumbs extends LightningElement {
 
-    @api
-    recordId;
-    dataId;
-    dataName;
+    @api recordId;
+    @track categoryId;
+    categoryName;
+    error;
     forYouLink;
+    dataName;
+
+    /**
+     * fetches product data
+     */
+    @wire(getRecord, { recordId:'$recordId', fields: [NAME_FIELD]})
+    product;
 
     connectedCallback() {
         this.iconhome = qutResourceImg + "/QUTImages/Icon/icon-home-blue.svg";
-        let currentUrl = window.location.href;
-        this.recordId = currentUrl.substr(currentUrl.length - 18);
+        this.init();   
+    } 
+    async init() {
+        this.recordId = await this.getRecordId();
         let productIds =  [
             this.recordId
         ];
         getProducts({
             productIds: productIds
         }).then(result => {
+            console.log('bread crumbs', result);
             if(result.productList.length > 0){
                 this.dataName = result.productList[0].childProdName;
             }
-        }).catch(
-            // do nothing
-        );
-        this.forYouLink = document.referrer;
-    } 
+        });
+        
+    }
+    getRecordId(){
+        let currentUrl = window.location.href;
+        return currentUrl.substr(currentUrl.length - 18);
+    }
+    get nameFind() {
+        if(this.dataName){
+                return this.dataName;
+        } else {
+            return '';
+        }
+    }
+    get forYouURL() {
+        if(this.categoryName && this.categoryId){
+            return basePath+'/category/'+this.categoryName.replaceAll(' ','-').toLowerCase()+'/'+this.categoryId.slice(0, -3);
+        } else {
+            return '#';
+        }
+    }
     changeTextDecorNone(event){
         const el = event.target;
         el.setAttribute('style', 'text-decoration:none');
@@ -37,5 +66,13 @@ export default class StoreBreadrumbs extends LightningElement {
     changeTextDecorUnderline(event){
         const el = event.target;
         el.setAttribute('style', 'text-decoration:underline');
+    }
+    @wire(getStudyProducts,{communityId:communityId})
+    handleGetStudyProducts(result){    
+        if(result.data){
+            this.categoryName = result.data[0].Name;
+            this.categoryId = result.data[0].Id;
+        } else {
+        }
     }
 }
