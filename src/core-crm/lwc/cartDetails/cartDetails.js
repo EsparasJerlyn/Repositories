0b29b76c1bb55136c2c38b34c48a7dfd78ee3770
Sebastianne Trias <sebastianne.trias@qut.edu.au>
2023-01-30 23:20:07 +0000

    Merged in hotfix/main/DEPP-5092_v2 (pull request #3037)
    DEPP-5092: Merge changes on DEPP-5091 and DEPP-5092
    
    * Merge changes on DEPP-5091 and DEPP-5092
    
    * Merged main into hotfix/main/DEPP-5092_v2
    
    Approved-by: Wushi Li

--- src/core-crm/lwc/cartDetails/cartDetails.js
+++ src/core-crm/lwc/cartDetails/cartDetails.js
@@ -20,6 +20,7 @@
 
 import { LightningElement, wire, api, track } from "lwc";
 import {getRecord, updateRecord, createRecord} from "lightning/uiRecordApi";
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import BasePath from "@salesforce/community/basePath";
 import userId from "@salesforce/user/Id";
@@ -50,6 +51,7 @@ import CART_PAYMENT_FIELD from '@salesforce/schema/WebCart.Cart_Payment__c';
 import { publish, MessageContext } from 'lightning/messageService';
 import payloadContainerLMS from '@salesforce/messageChannel/Breadcrumbs__c';
 
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
 //Contact fields
 const CONTACT_FIELDS = [
   "User.ContactId",
@@ -87,6 +89,7 @@ export default class CartDetails extends LightningElement {
   @track disablePayment = true;
   @track showStaffId;
   @track showStudentId;
+  registeredEmailHelpText = "";
 
   isFreeOnly;
   @track cartItems = [];
@@ -258,6 +261,13 @@ export default class CartDetails extends LightningElement {
     }
   }
 
+  @wire(getObjectInfo, { objectApiName: CONTACT_OBJECT })
+  wiredObjectContact({ error, data }){
+    if(data){
+      this.registeredEmailHelpText = data.fields.Registered_Email__c.inlineHelpText;
+    }
+  }
+  
   //get contact data
   @wire(getRecord, { recordId: userId, fields: CONTACT_FIELDS })
   wiredContact({ error, data }) {
