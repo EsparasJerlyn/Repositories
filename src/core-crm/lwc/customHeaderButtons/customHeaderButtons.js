58e795c497cdcbc81ffcb6a3196dd85d8d564382
Nicole Genon <nicole.genon@qut.edu.au>
2024-01-15 13:59:54 +0000

    Merged in feature/develop_secondary/DEPP-6966 (pull request #4866)
    DEPP-6966-7. Closed Stage
    
    * Merged develop_secondary into feature/develop_secondary/DEPP-6966
    * Added DynamicDataTable on Relationships tab in Distributed List Record Page
    
    * Merged develop_secondary into feature/develop_secondary/DEPP-6966
    * Added filter to customHeaderDatatable in Distributed List Record Page
    
    * Merged develop_secondary into feature/develop_secondary/DEPP-6966
    * Added disabledButton function
    
    * Updated isDisabledButton function
    
    
    Approved-by: Adam Best
    Approved-by: Rhea Torres

--- src/core-crm/lwc/customHeaderButtons/customHeaderButtons.js
+++ src/core-crm/lwc/customHeaderButtons/customHeaderButtons.js
@@ -4,11 +4,35 @@
  * @author Accenture
  * 
  * @history
- *    | Developer                 | Date                  | JIRA                 | Change Summary               |
-      |---------------------------|-----------------------|----------------------|------------------------------|
-      | marygrace.li@qut.edu.au   | December 19, 2023     | DEPP-7489            | Created file                 | 
-      |                           |                       |                      |                              | 
+ *    | Developer                 | Date                  | JIRA                 | Change Summary                                   |
+      |---------------------------|-----------------------|----------------------|--------------------------------------------------|
+      | marygrace.li@qut.edu.au   | December 19, 2023     | DEPP-7489            | Created file                                     | 
+      | nicole.genon@qut.edu.au   | January 15, 2024      | DEPP-6966            | Added wiredList and isDisabledButton             | 
  */
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord  } from 'lightning/uiRecordApi';
+import LIST_STAGE from '@salesforce/schema/List__c.Stage__c';
 
-export default class CustomHeaderButtons extends LightningElement {}
+export default class CustomHeaderButtons extends LightningElement {
+     @api recordId;
+     @api selectedRows;
+     statusSelected = 'Close';
+     stageValue;
+
+     listMemberColumns = [LIST_STAGE];
+
+     @wire(getRecord, { recordId: "$recordId", fields: "$listMemberColumns" })
+     wiredList(responseData) {
+          const { data, error } = responseData;
+
+          if (data) {
+               const fields = data.fields;
+               this.stageValue = fields.Stage__c.value;
+          }
+
+     }
+
+     get isDisabledButton() {
+          return this.stageValue === "Distribute" || this.stageValue === 'Closed' ? true : false;
+     }
+}
