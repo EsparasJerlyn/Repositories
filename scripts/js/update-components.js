fe967e1c666d7262dbdb366fdf80bc7f8956d3f2
Adam Best <adam.best@salesforce.com>
2024-04-10 13:11:20 +1000

    remove console log entries

--- scripts/js/update-components.js
+++ scripts/js/update-components.js
@@ -17,8 +17,6 @@ function exec(cmd, options) {
 const shouldFieldBeAddedToPermissionSet = function(field_file_Result){
     const fieldRequired = field_file_Result?.CustomField?.required?.[0] ?? null;
     const fieldType = field_file_Result?.CustomField?.type?.[0] ?? null;
-    //console.log(`fieldType: ${fieldType}`);
-    //console.log(`fieldRequired: ${fieldRequired}`);
     if(fieldType != 'MasterDetail' && fieldRequired != 'true'){
         return true;
     }
@@ -27,7 +25,6 @@ const shouldFieldBeAddedToPermissionSet = function(field_file_Result){
 
 const shouldFieldBeReadOnly = function(new_field){
     if(new_field.fieldType == 'Summary' || new_field.fieldFormula){
-        //console.log(`shouldFieldBeReadOnly = true`);
         return true;
     }
     return false;
@@ -36,13 +33,10 @@ const shouldFieldBeReadOnly = function(new_field){
 const start = async function(){
     
     const diff = exec(`git diff --cached --name-only --diff-filter=ACMRTUXB`, {trim: true});
-    //console.log('diff: ',diff)
     const lines = diff.split("\n");
-    //console.log('lines: ',lines)
 
     let new_fields = [];
     for (const line of lines){
-        //console.log('line: ',line)
         let match = line.match('(.+)(objects)(\/)(.+)(\/)(fields\/)(.+)(\.field-meta.xml)');
         if(!match) {
             continue;
@@ -87,9 +81,7 @@ const start = async function(){
         }
 
         for(const new_field of new_fields){
-            
             console.log(`Adding field: ${new_field.ObjectPlusField} to ${permissionSet}`);
-
             var field_permission;
             var fieldReadWriteStatus = 'read'
 
