e554c041b15790cf70985d8b8f6359a0f79715d3
Kim Howard Capanas <kimhoward.capanas@qut.edu.au>
2024-04-11 03:44:39 +0000

    Merged in feature/IT3P2/DEPP-8203 (pull request #5722)
    DEPP-8203: 5. Donation Cause - Public Release Tab
    
    * update lwc
    
    * Merged project/IT3P2 into feature/IT3P2/DEPP-8203
    * Merged project/IT3P2 into feature/IT3P2/DEPP-8203
    * update permission set
    
    * Merge branch 'feature/IT3P2/DEPP-8203' of https://bitbucket.org/qutdev/sfdx into feature/IT3P2/DEPP-8203
    
    * update lwc
    
    * Merged project/IT3P2 into feature/IT3P2/DEPP-8203
    * Update permission sets
    
    * Merge branch 'feature/IT3P2/DEPP-8203' of https://bitbucket.org/qutdev/sfdx into feature/IT3P2/DEPP-8203
    
    * Merged project/IT3P2 into feature/IT3P2/DEPP-8203
    
    
    Approved-by: Ryan Joshua Dela Cruz

--- src/core-crm/lwc/dynamicRecordEditForm/dynamicRecordEditForm.js
+++ src/core-crm/lwc/dynamicRecordEditForm/dynamicRecordEditForm.js
@@ -8,163 +8,228 @@
  *    | Developer Email                | Date                  | JIRA                   | Change Summary                  |
  *    |--------------------------------|-----------------------|------------------------|---------------------------------|
  *    | ryan.j.a.dela.cruz             | February 2, 2024      | DEPP-6950              | Created file                    |
+ *    | kim.howard.capanas             | March 18, 2024        | DEPP-8203              | Added dynamic filters and logic parameter support for flexipage                 |
  *    |                                |                       |                        |                                 |
  */
 
-import { LightningElement, api, wire } from "lwc";
-import { reduceErrors } from "c/lwcUtility";
-import customForm from "@salesforce/resourceUrl/CustomRecordEditForm";
+import { LightningElement, api, wire, track } from "lwc";
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+import {publish,subscribe,APPLICATION_SCOPE,MessageContext} from 'lightning/messageService';
 import { loadStyle } from "lightning/platformResourceLoader";
-import { getObjectInfo } from "lightning/uiObjectInfoApi";
-import LIST_OBJECT from "@salesforce/schema/List__c";
+import customForm from "@salesforce/resourceUrl/CustomRecordEditForm";
+import getRecordIds from "@salesforce/apex/DynamicRecordEditFormCtrl.getRecordIds";
+import getUiBehavior from "@salesforce/apex/DynamicRecordEditFormCtrl.getUiBehavior";
+import getContentBlockFilter from "@salesforce/apex/DynamicRecordEditFormCtrl.getContentBlockFilter";
+import donationCauseMessageChannel from '@salesforce/messageChannel/DonationCause__c';
 
 export default class DynamicRecordEditForm extends LightningElement {
-  @api recordObjectApiName; // FOR DELETION
-  @api fieldListToOverride;
-  @api passedObjectApiName;
-  @api passedRecordId;
-  @api column1FieldList;
-  @api column2FieldList;
-  @api hiddenFieldList;
-  @api showEditField;
+  // start of params
+  @api childObjectRecord;
+  @api relatedListLabel;
+  @api numberOfColumn;
+  @api column1;
+  @api column2;
+  @api recordId;
+  @api isSubscriber;
+  @api contentBlockFilter;
+  @api readOnlyByParentField;
+  @api readOnlyByUser;
+  @api objectApiName;
 
-  detailRecordId;
-  error;
+
+  // @track data;
+  @track showContentBlock;
+  @track passedRecordId;
+  @track uiSectionBehavior;
+
+  showEditField = true;
+  showFooter;
+  messageReceived = '';
+  
+  setParameters() {
+    let paramsMap = {};
+    paramsMap["recordId"] = this.recordId;
+    paramsMap["parentRecord"] = 'Donation_Cause__c';
+    paramsMap["visibilityByUser"] = this.readOnlyByUser;
+    paramsMap["visibilityByParent"] = this.readOnlyByParentField
+    return paramsMap;
+  }
+
+  setParametersForVisibilityFilter() {
+    let paramsMap = {};
+    paramsMap["recordId"] = this.passedRecordId;
+    paramsMap["childRecord"] = this.childObjectRecord;
+    paramsMap["contentBlockFilter"] = this.contentBlockFilter;
+    return paramsMap;
+  }
 
   renderedCallback() {
     Promise.all([loadStyle(this, customForm)]).then(() => {});
   }
 
-  handleEdit() {
-    // Dispatch showEditField custom event to parent before negation
-    this.dispatchEvent(
-      new CustomEvent("showfooter", {
-        detail: {
-          message: this.showEditField
-        }
-      })
-    );
+  @wire(MessageContext)
+  messageContext
 
+  getRecordId(){
+    return new Promise((resolve) => {
+      getRecordIds({
+        recordId: this.recordId,
+        objectApiName : this.childObjectRecord
+      })
+      .then((result) =>{
+        this.passedRecordId = result;
+        resolve();
+      })
+      .catch((error) => {
+        const logger = this.template.querySelector("c-logger");
+        if (logger) {
+          logger.error(
+            "Exception caught in method getRecordId in LWC dynamicRecordEditForm: ",
+            JSON.stringify(error)
+          );
+          logger.saveLog();
+        }
+        resolve();
+      });
+    });
+  }
+
+  get reactiveParentId(){
+    return this.objectApiName + '.Id';
+  } 
+
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: ["$reactiveParentId"],
+  })
+  handleEditBehavior(result){
+    if (result.data) {
+      this.getUiBehavior();
+    }
+  }
+  
+  async connectedCallback() {
+    await this.getRecordId(); // passsedRecordId has value
+    this.getUiBehavior(); // controls edit button
+    this.getContentBlockFilter(); // section visibility filter
+    if (this.isSubscriber){
+      this.subscribeToMessageChannel();
+    }
+  }
+
+  handleEdit() {
     this.showEditField = !this.showEditField;
   }
 
-  _recordTypeId;
-  @wire(getObjectInfo, { objectApiName: LIST_OBJECT })
-  objectInfo({ data, error }) {
-    if (data) {
-      const recordTypes = data.recordTypeInfos;
-      this._recordTypeId = Object.keys(recordTypes).find(
-        (key) => recordTypes[key].name === "Engagement Opportunity"
-      );
-    } else if (error) {
-      // Handle error
-      console.error("Error retrieving object information", error);
-    }
-  }
-
-  get recordTypeId() {
-    return this._recordTypeId;
-  }
-
-  @api async submit() {
-    const inputFields = this.template.querySelectorAll("lightning-input-field");
-    const fields = {};
-
-    // Initialize or retrieve the session object
-    let sessionData = sessionStorage.getItem("combinedFields");
-    const combinedFields = sessionData ? JSON.parse(sessionData) : {};
-
-    inputFields.forEach((field) => {
-      fields[field.fieldName] = field.value;
-    });
-
-    // Merge current fields with the existing combinedFields
-    const sessionFields = { ...combinedFields, ...fields };
-
-    // Save the updated combinedFields in the session
-    sessionStorage.setItem("combinedFields", JSON.stringify(sessionFields));
-
-    if (this.hiddenFieldList) {
-      const url = window.location.href;
-      this.detailRecordId = url.split("/r/")[1].split("/")[1];
-
-      this.hiddenFieldList.forEach((hiddenField) => {
-        switch (hiddenField.name) {
-          case this.fieldListToOverride.engagementOpportunityId:
-            fields[hiddenField.name] = this.detailRecordId;
-            break;
-          case this.fieldListToOverride.listName:
-            fields[hiddenField.name] =
-              sessionFields.Engagement_Opportunity_Name__c;
-            break;
-          case this.fieldListToOverride.listPurpose:
-            fields[hiddenField.name] = sessionFields.Summary__c;
-            break;
-          case this.fieldListToOverride.recordTypeId:
-            fields[hiddenField.name] = this.recordTypeId;
-            break;
-          default:
-            fields[hiddenField.name] = hiddenField.value;
-        }
-      });
-    }
-
-    console.log("[1] Actual Submit: ", JSON.parse(JSON.stringify(fields)));
-
-    // Initialize or retrieve the session object
-    let targetForm = this.template.querySelector("lightning-record-edit-form");
-    targetForm.submit(fields);
-  }
-
-  handleSuccess(event) {
-    event.preventDefault();
-    console.log(
-      "[2] Record saved (",
-      event.detail.apiName,
-      "):",
-      event.detail.id
-    );
-
-    if (event.detail.apiName === "List__c") {
-      console.log("[3] recordidupdate");
-
-      this.dispatchEvent(
-        new CustomEvent("recordidupdate", {
-          detail: {
-            message: event.detail.id
-          }
-        })
-      );
-    } else {
-      // console.log("[3] success");
-      // this.dispatchEvent(
-      //   new CustomEvent("success", {
-      //     detail: {
-      //       message: true
-      //     }
-      //   })
-      // );
-    }
-
-    this.showEditField = true;
-    this.dispatchEvent(
-      new CustomEvent("showfooter", {
-        detail: {
-          message: false
-        }
+  getUiBehavior() {
+    return new Promise((resolve) => {
+      getUiBehavior({
+        paramsMap: this.setParameters()
       })
-    );
+      .then((result) => {
+        this.uiSectionBehavior = result;
+        resolve();
+      })
+      .catch((error) => {
+        const logger = this.template.querySelector("c-logger");
+        if (logger) {
+          logger.error(
+            "Exception caught in method getUiBehavior in LWC dynamicRecordEditForm: ",
+            JSON.stringify(error)
+          );
+          logger.saveLog();
+        }
+        resolve();
+      });
+    })
   }
+
+  getContentBlockFilter() {
+    getContentBlockFilter({
+      paramsMap: this.setParametersForVisibilityFilter()
+    })
+    .then((result) => {
+      this.showContentBlock = result;
+    })
+    .catch((error) => {
+      const logger = this.template.querySelector("c-logger");
+      if (logger) {
+        logger.error(
+          "Exception caught in method getContentBlockFilter in LWC dynamicRecordEditForm: ",
+          JSON.stringify(error)
+        );
+        logger.saveLog();
+      }
+    });
+  }
+
+  subscribeToMessageChannel(){
+    this.subscription = subscribe(
+      this.messageContext, 
+      donationCauseMessageChannel, 
+      (message) => {
+        this.handleMessage(message)
+      },
+      {
+        scope :APPLICATION_SCOPE
+      }
+    )
+  }
+
+  handleMessage(message){
+    this.messageReceived = message.lmsData.data ? message.lmsData.data : 'No data receieved';
+    this.getUiBehavior();
+    this.getContentBlockFilter();
+  }
+
+  handleSuccess() {
+    this.showEditField = !this.showEditField;
+    let lmsMessage = 'Content Block'; // update lmsMessage value for future use.
+    const message = {
+      lmsData : {
+        data : lmsMessage
+      }
+    }
+    publish(this.messageContext, donationCauseMessageChannel, message);
+  }
+
+  handleCancel(event) {
+    this.showFooter = false;
+    this.showEditField = !this.showFooter;
+  }
+
+  updateShowFooter(event) {
+    this.showFooter = event.detail.message;
+    this.showEditField = !this.showFooter;
+  }
+
 
   handleError(error) {
-    console.log("ERROR Reduced: ", reduceErrors(error).join(", "));
-    sessionStorage.setItem("dynamicErrors", reduceErrors(error).join(", "));
-    this.dispatchEvent(
-      new CustomEvent("error", {
-        detail: {
-          message: error
-        }
-      })
-    );
+    // If there are additional builds in the future, such as adding a toast message when an error occurred, we can add it here.
+  }
+
+  get fetchColumn1() {
+    return this.numberOfColumn >= 1 ? JSON.parse(this.column1) : "";
+  }
+
+  get fetchColumn2() {
+    this.checker >= 2;
+    return this.numberOfColumn >= 2 ? JSON.parse(this.column2) : "";
+  }
+
+  get sectionFilter(){
+    return this.showContentBlock;
+  }
+
+  set sectionFilter(value){
+    this.showContentBlock = value;
+  }
+
+  get editFilter(){
+    return this.uiSectionBehavior;
+  }
+
+  set editFilter(value){
+    this.uiSectionBehavior = value;
   }
 }
