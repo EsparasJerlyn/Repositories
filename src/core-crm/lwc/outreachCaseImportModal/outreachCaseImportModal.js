69390a6e5fb22d7e8128924749bb21c84b62453f
Vincent <vincent.ferrer@qut.edu.au>
2024-05-10 14:19:00 +0800

    DEPP-7526 - Updated Wireframe 2

--- src/core-crm/lwc/outreachCaseImportModal/outreachCaseImportModal.js
+++ src/core-crm/lwc/outreachCaseImportModal/outreachCaseImportModal.js
@@ -1,7 +1,9 @@
 import { LightningElement,track } from 'lwc';
 
 export default class OutReachCaseImportModal extends LightningElement {
+  
   @track modalOpen = true;
+  @track isCreateButtonDisabled = true;
 
   closeModal() {
     const closeModalEvent = new CustomEvent('closemodal');
@@ -15,4 +17,12 @@ export default class OutReachCaseImportModal extends LightningElement {
   get backdropClass() {
     return this.modalOpen ? 'slds-backdrop slds-backdrop_open' : 'slds-backdrop slds-backdrop_close';
   }
+
+  handleFileChange(event) {
+    if (event.target.files.length > 0) {
+        this.isCreateButtonDisabled = false; 
+    } else {
+        this.isCreateButtonDisabled = true;
+    }
+}
 }
